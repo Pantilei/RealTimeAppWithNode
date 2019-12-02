21000f595b117201f2a90a4ed7a4e828d09ce1fd
Pantilei IAnulo <ianulov.pantilei@gmail.com>
2019-12-02 01:35:06 +0000

    Contact page complited, email sending implemented

---
+++ config.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  mailer: {
+    service: "Gmail",
+    auth: {
+      user: "ianulov.pantilei@gmail.com",
+      pass: "Panian007"
+    }
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,11 @@
 var express = require("express");
 var router = express.Router();
 
+var nodemailer = require("nodemailer");
+var config = require("../config");
+
+var transporter = nodemailer.createTransport(config.mailer);
+
 /* GET home page. */
 router.get("/", function(req, res, next) {
   res.render("index", { title: "ColaborativeCode" });
@@ -9,5 +14,41 @@ router.get("/", function(req, res, next) {
 router.get("/about", function(req, res, next) {
   res.render("about", { title: "ColaborativeCode" });
 });
+
+router
+  .route("/contact")
+  .get(function(req, res, next) {
+    res.render("contact", { title: "ColaborativeCode" });
+  })
+  .post(function(req, res, next) {
+    req.checkBody("name", "Empty Name").notEmpty();
+    req.checkBody("email", "Empty Email").isEmail();
+    req.checkBody("message", "Empty Message").notEmpty();
+
+    var errors = req.validationErrors();
+
+    if (errors) {
+      res.render("contact", {
+        name: req.body.name,
+        email: req.body.email,
+        message: req.body.message,
+        errorMessages: errors
+      });
+    } else {
+      var mailOptions = {
+        from: "ColabarativeCode",
+        to: "ianulov.pantilei@gmail.com",
+        subject: "New message from visitor",
+        text: req.body.message
+      };
+
+      transporter.sendMail(mailOptions, function(error, info) {
+        if (error) {
+          return console.log(error);
+        }
+        res.render("thank", { title: "ColaborativeCode" });
+      });
+    }
+  });
 
 module.exports = router;
